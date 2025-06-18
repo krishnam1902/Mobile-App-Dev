6f39fa1136d1bae160f16b522e5fae6ef6cb160c
raghavurbannest <raghav.gupta@urbannest.ai>
2025-06-18 17:22:23 +0530

    initial commit

---
+++ shop/src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(shop)" options={{ headerShown: false, title: 'Shop' }} />
+      <Stack.Screen name="categories" options={{ headerShown: false, title: 'Categories' }} />
+      <Stack.Screen name="products" options={{ headerShown: true, title: 'Products' }} />
+      <Stack.Screen name="cart" options={{ presentation: "modal", title: 'Shopping' }} />
+     <Stack.Screen name="auth" options={{ headerShown: true }} />
+
+      
+    </Stack>
+  );
+}
---
+++ shop/src/app/auth.tsx
@@ -0,0 +1,12 @@
+import { View ,Text} from "react-native";
+
+export default function Auth(){
+    return(
+        <view>
+            <Text>
+                auth
+            </Text>
+        </view>
+    )
+     
+}
