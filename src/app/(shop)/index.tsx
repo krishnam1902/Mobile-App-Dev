26ef42a3bce3d57c9a9ccf01dc3dd5324ecab1b2
krishnam1902 <krishnam.anand@urbannest.ai>
2025-06-18 13:58:48 +0530

    Changes post Supabase integration

---
+++ src/app/(shop)/index.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, StyleSheet, StatusBar } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home
+
+const styles = StyleSheet.create({})
---
+++ src/app/auth.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native';
+
+export default function Cart() {
+    return (<View>
+        <Text>Auth</Text>
+    </View>);
+}
