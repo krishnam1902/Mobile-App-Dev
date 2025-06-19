0d17d32631f14d6a2004d0ca48da334b32943a6a
raghavurbannest <raghav.gupta@urbannest.ai>
2025-06-19 20:25:06 +0530

    Update auth.tsx

--- shop/src/app/auth.tsx
+++ shop/src/app/auth.tsx
@@ -1,12 +1,1 @@
-import { View ,Text} from "react-native";
 
-export default function Auth(){
-    return(
-        <view>
-            <Text>
-                auth
-            </Text>
-        </view>
-    )
-     
-}
