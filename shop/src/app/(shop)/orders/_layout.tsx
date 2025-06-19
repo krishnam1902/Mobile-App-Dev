fc715516960e683e382b6686c98ef6fad4885e1b
raghavurbannest <raghav.gupta@urbannest.ai>
2025-06-19 20:23:16 +0530

    Update _layout.tsx

--- shop/src/app/(shop)/orders/_layout.tsx
+++ shop/src/app/(shop)/orders/_layout.tsx
@@ -1,10 +1,2 @@
-import { Stack } from "expo-router";
 
-export default function OrdersLayout(){
-    return(
 
-        <Stack>
-            <Stack.Screen  name="index" options={{headerShown:false}}/>
-        </Stack>
-    )
-}
